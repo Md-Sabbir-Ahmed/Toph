89fd74e9da045f73974293eecb468713609da8de
Sabbir <sabbir106706@gmail.com>
2020-05-11 00:05:00 +0600

    p4 using cpp

---
+++ Pie Are Squared/Pie Are Squared.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    float r,A;
+    cin>>r;
+    A=M_PI*r*r;
+    cout<<A;
+
+    return 0;
+}
+
+
