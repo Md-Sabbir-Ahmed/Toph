c2c55c57442c8f89151615c440657dfe1404bf15
Sabbir <sabbir106706@gmail.com>
2020-05-11 00:06:33 +0600

    p5 using cpp

---
+++ Missing Number/Missing Number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int sum,a,b,c,d;
+    cin>>sum;
+    cin>>a>>b>>c;
+    cout<<sum-(a+b+c);
+
+    return 0;
+}
+
+
