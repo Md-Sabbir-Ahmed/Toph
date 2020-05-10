5b18de6b8c0eed87e3b902513ebef8b07a5b3bd6
Sabbir <sabbir106706@gmail.com>
2020-05-11 00:03:06 +0600

    p3 using cpp

---
+++ Squared/Squared.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int L;
+    cin>>L;
+    cout<<L*L;
+
+    return 0;
+}
+
+
