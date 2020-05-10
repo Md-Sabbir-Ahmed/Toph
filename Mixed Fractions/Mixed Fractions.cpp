4af89ad8df44bdbff67877de594c397b40c51df0
Sabbir <sabbir106706@gmail.com>
2020-05-11 00:08:50 +0600

    p7 using cpp

---
+++ Mixed Fractions/Mixed Fractions.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b,x,y;
+    cin>>a>>b;
+    x=a/b;
+    y=a%b;
+    cout<<x<<" "<<y<<" "<<b;
+
+    return 0;
+}
+
+
