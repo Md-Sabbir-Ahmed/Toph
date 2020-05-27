d42f81e557b05155bd9edbb567bd86bba3f6b478
Sabbir <sabbir106706@gmail.com>
2020-05-27 12:14:04 +0600

    p24 using cpp

---
+++ Making Friends/Making Friends.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m=0;
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+    	if(n%i==0)
+    	{
+    		m++;
+    	}
+    }
+    cout<<m+1;
+    return 0;
+}
+
